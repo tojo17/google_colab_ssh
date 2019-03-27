7dd8fa8d7487488ea9be81a4f05913bcd2a35d82
Sykie Chen <chen17@coder17.com>
2019-03-27 20:28:42 +0800

    init as git repo

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# google_colab_ssh
-initialize Google Colab for ssh connection
+# Google Colab ssh starter
+initialize Google Colab for ssh connection via frp
+
+Code in iPython:
+``` python
+! curl "myip.ipip.net"
+
+# frp config file
+print "### writing frp config file"
+ini = '''
+[common]
+server_addr = < frp server >
+server_port = < frp port >
+log_file = ./frpc.log
+log_level = warn
+log_max_days = 3
+token = < frp server token >
+
+[google_colab_ssh_23333]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 23333
+remote_port = < port you like >
+'''
+with open("frpc.ini", "w") as f:
+    f.write(ini)
+
+# ssh keys
+print "### writing ssh keys"
+keys = '''
+< ssh public keys >
+'''
+with open("authorized_keys", "w") as f:
+    f.write(keys)
+
+# download init script
+print "### get init script and run"
+! wget -q --show-progress -c https://raw.githubusercontent.com/SykieChen/google_colab_ssh/master/colab_init.sh
+! bash colab_init.sh < your user password >
+```
---
+++ colab_init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# there should already be frpchome.ini and authorized_keys files here
+# usage: bash colab_init.sh <password_of_new_user>
+
+# make new user
+echo "### making new user with password $1"
+adduser -q colab --gecos "Google Colab" --disabled-password
+echo "colab:$1" | chpasswd
+usermod -aG sudo colab
+
+
+# configure ssh
+echo "### configuring ssh"
+mkdir -p /home/colab/.ssh
+mv authorized_keys /home/colab/.ssh/authorized_keys
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "Port 23333" >> /etc/ssh/sshd_config
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+echo "AuthorizedKeysFile %h/.ssh/authorized_keys" >> /etc/ssh/sshd_config
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /home/colab/.bashrc
+echo "export LD_LIBRARY_PATH" >> /home/colab/.bashrc
+service ssh restart
+
+# download frp
+echo "### downloading frp"
+wget -q --show-progress -c https://github.com/fatedier/frp/releases/download/v0.25.2/frp_0.25.2_linux_amd64.tar.gz
+tar xzf frp_0.25.2_linux_amd64.tar.gz -C /opt/
+mv /opt/frp_0.25.2_linux_amd64 /opt/frp
+mv frpchome.ini /opt/frp/frpchome.ini
+chmod -R 755 /opt/frp
+echo "### start frp"
+nohup /opt/frp/frpc -c /opt/frp/frpchome.ini &
+
+# install packages
+echo "### installing packages"
+apt-get -qq update
+# apt install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen
+apt-get -qq install net-tools tmux
+# echo y | unminimize
+
+echo "### done"
